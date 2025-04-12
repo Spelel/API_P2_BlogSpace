76a7a5dcef5905e5bbcde3e312e83b5a4a4ce0a9
Arek <arek-karczo.wp.pl@wp.pl>
2025-04-12 19:32:39 +0200

    some styling

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
 .then(data => {
     const postArr = data.slice(0, 5)
     postArr.forEach(post => document.getElementById("postHandler").innerHTML+=`
-        <p class="postTitle">${post.title}</p> 
+        <h2 class="postTitle">${post.title}</h2> 
         <p class="postBody">${post.body}</p>
+        <hr />
     `)
 })
