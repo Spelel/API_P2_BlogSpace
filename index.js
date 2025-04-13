b1e76d9116049066c40ea7723a8bda11f916d777
Arek <arek-karczo.wp.pl@wp.pl>
2025-04-13 13:19:17 +0200

    new form, button and some JS

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-.then((response) => response.json())
-.then(data => {
-    const postArr = data.slice(0, 5)
-    postArr.forEach(post => document.getElementById("postHandler").innerHTML+=`
+let postsArray = []
+
+function renderPosts () {
+    let html = ""
+    for (let post of postsArray) {
+        html +=`
         <h2 class="postTitle">${post.title}</h2> 
         <p class="postBody">${post.body}</p>
         <hr />
-    `)
+    `}
+    document.getElementById("postHandler").innerHTML = html
+}
+
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+.then((response) => response.json())
+.then(data => {
+    postsArray = data.slice(0, 5)
+    renderPosts()
 })
+
+
+
+document.addEventListener('submit', function(e){           
+    e.preventDefault()
+    const newPost = document.getElementById("newPostTitle").value
+    const newBody = document.getElementById("newPostBody").value
+
+    const post = {
+        title: newPost,
+        body: newBody
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify (post)
+    })
+    .then(res => res.json())
+    .then(data => {
+        postsArray.unshift(data)
+        renderPosts()
+        document.getElementById("newPostTitle").value = ''
+        document.getElementById("newPostBody").value = ''
+    })
+})
+
